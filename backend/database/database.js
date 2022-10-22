795fd7e83bd534229c7774ca35db5673f56ea648
GulcanC <gulcancoskun.a@gmail.com>
2022-10-23 00:26:06 +0200

    edit .env

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,11 +1,32 @@
-const dotenv = require("dotenv").config();
+/* const dotenv = require("dotenv").config();
 
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
+}; */
+
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: process.env.DIALECT,
+});
+
+db.connect(function (error) {
+  if (error) {
+    console.log(" ⛔️ Can not be connected to the data base sql!");
+    throw error;
+  } else {
+    console.log(" ✅ Connected to the data base sql! ");
+  }
+});
+
+module.exports.getDB = () => {
+  return db;
 };
