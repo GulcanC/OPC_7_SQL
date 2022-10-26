ba490cb221ef51bc44cb05653ba16fcc24cdf17b
GulcanC <gulcancoskun.a@gmail.com>
2022-10-26 14:58:34 +0200

    connect to database

--- backend/app.js
+++ backend/app.js
@@ -3,20 +3,26 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const Sequilize = require("sequelize");
-const sequilize = new Sequilize("groupmania_sql", "root", "1571256,Gaps", {
-  host: "localhost",
-  dialect: "mysql",
-});
+require("dotenv").config({ path: "./vars/.env" });
+
+const sequilize = new Sequilize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  }
+);
 
 sequilize
   .authenticate()
   .then(() => {
-    console.log("yes");
+    console.log("✅ Connected to the sql database!");
   })
   .catch((err) => {
-    console.log("no");
+    console.log("⛔️ Fail to connect to the sql database!");
   });
-
 const app = express();
 
 app.use(cors());
