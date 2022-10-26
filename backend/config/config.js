af1922ccbea8f0e16d9a574afb169f71d8f39569
GulcanC <gulcancoskun.a@gmail.com>
2022-10-26 15:30:23 +0200

    problem

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: "./vars/.env" });
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
@@ -8,27 +9,3 @@ module.exports = {
     dialect: process.env.DIALECT,
   },
 };
-
-/* const mysql = require("mysql");
-
-const db = mysql.createConnection({
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  dialect: process.env.DIALECT,
-  host: process.env.HOST,
-});
-
-db.connect(function (error) {
-  if (error) {
-    console.log(" ⛔️ Can not be connected to the data base sql!");
-    throw error;
-  } else {
-    console.log(" ✅ Connected to the data base sql! ");
-  }
-});
-
-module.exports.getDB = () => {
-  return db;
-};
- */
