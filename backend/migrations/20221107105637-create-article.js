e3d9851792710d07356ff2e21203c8f8be31dec6
GulcanC <gulcancoskun.a@gmail.com>
2022-11-07 12:40:10 +0100

    edit comment model

--- backend/migrations/20221107105637-create-article.js
+++ backend/migrations/20221107105637-create-article.js
@@ -1,34 +1,46 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Articles', {
+    await queryInterface.createTable("Articles", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       content: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Articles');
-  }
+    await queryInterface.dropTable("Articles");
+  },
 };
--- backend/models/article.js
+++ backend/models/article.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Article extends Model {
     /**
@@ -11,15 +11,32 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Article.hasMany(models.Comment, {
+        as: "comment",
+        onDelete: "cascade",
+        hooks: true,
+        foreignKey: "articleId",
+      });
+
+      Article.hasMany(models.Like, {
+        as: "like",
+        onDelete: "cascade",
+        hooks: true,
+        foreignKey: "articleId",
+      });
+      Article.belongsTo(models.User, { as: "user", foreignKey: "userId" });
     }
   }
-  Article.init({
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Article',
-  });
+  Article.init(
+    {
+      title: { type: DataTypes.STRING, allowNull: false },
+      content: { type: DataTypes.TEXT, allowNull: false },
+      image: { type: DataTypes.STRING, allowNull: true },
+    },
+    {
+      sequelize,
+      modelName: "Article",
+    }
+  );
   return Article;
 };
