2540c914f3e624b1d217c911d4de085b75ba5d38
GulcanC <gulcancoskun.a@gmail.com>
2022-11-14 11:34:51 +0100

    create content validation for article

---
+++ backend/validation/article-validation.js
@@ -0,0 +1,23 @@
+const Yup = require("yup");
+const catchAsync = require("../middleware/asyncHandler");
+
+const articleSchema = Yup.object({
+  title: Yup.string()
+    .trim()
+    .required("⚠️ Provide a title!")
+    .min(3, "⚠️ The title must contain at least 3 characters!")
+    .max(100, "⚠️ The title must contain max. 100 characters!"),
+  content: Yup.string()
+    .trim()
+    .required('⚠️ Write something for your article!"')
+    .min(5, "⚠️ The title must contain at least 3 characters!")
+    .max(1000, "⚠️ The title must contain max. 100 characters!"),
+});
+exports.articleValidation = catchAsync(async (req, res, next) => {
+  const value = await loginSchema.validate(
+    { ...req.body },
+    { abortEarly: false, stripUnknown: true }
+  );
+  req.body = value;
+  next();
+});
