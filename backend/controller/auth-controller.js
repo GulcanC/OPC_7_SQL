ce98546243184429ae5f0f15597614951100d2d6
GulcanC <gulcancoskun.a@gmail.com>
2022-12-02 00:11:26 +0100

    create login function and router

--- backend/controller/auth-controller.js
+++ backend/controller/auth-controller.js
@@ -14,29 +14,80 @@ exports.signup = asyncHandler(async (req, res, next) => {
   const emailCryptoJS = cryptojs
     .HmacSHA256(req.body.email, process.env.EMAIL_CRYPTO)
     .toString();
-  console.log(cryptojs);
+  // console.log(cryptojs);
 
   // Hash password before sending to the database
   const hashPassword = await bcrypt.hash(req.body.password, 10);
 
   const { email, ...userData } = req.body;
 
-  const [user, created] = await User.findOrCreate({
+  //https://sebhastian.com/sequelize-findorcreate/
+
+  // Use sequelize findOrCreate function
+  User.findOrCreate({
     where: { email: emailCryptoJS },
     defaults: {
       ...userData,
       password: hashPassword,
       email: emailCryptoJS,
     },
+  }).then(([user, created]) => {
+    console.log(created);
+    if (created) {
+      res.status(201).json({ message: "✅ User created!" });
+    } else {
+      return next(
+        new AppError(
+          "This account already exist, please sign in or use a new email address",
+          400
+        )
+      );
+    }
   });
+});
 
-  if (!created) {
-    return next(
-      new AppError(
-        "This account already exist, please sign in or use a new email address",
-        400
-      )
-    );
-  }
-  res.status(201).json({ message: "✅ User created!" });
+exports.login = asyncHandler(async (req, res, next) => {
+  console.log("🎉🎉🎉USER LOGIN🎉🎉🎉");
+  console.log(req.body);
+  const emailCryptoJS = cryptojs
+    .HmacSHA256(req.body.email, process.env.EMAIL_CRYPTO)
+    .toString();
+  // console.log(cryptojs);
+
+  // check whether crypto email exists in the database
+  User.findOne({
+    where: { email: emailCryptoJS },
+  })
+    .then((user) => {
+      // if the value is null, user does not exist in the database
+      if (user === null) {
+        res
+          .status(401)
+          .json({ message: "⛔️ Account does not exist! Please register!" });
+        // if we have a value, compare the password which is in the database and user password
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res
+                .status(401)
+                .json({ message: "⛔️ Email and password do not match" });
+            }
+            // if password is correct we have userId and token
+            else if (valid) {
+              res.status(200).json({
+                message: "✅ User login is successful!",
+                token: jwt.sign(
+                  { userId: user._id },
+                  process.env.JWT_KEY_TOKEN,
+                  { expiresIn: "24h" }
+                ),
+              });
+            }
+          })
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
 });
--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/auth-controller");
 const { signupValidation } = require("../validation/signup-validation");
+const { loginValidation } = require("../validation/login-validation");
 
 router.post("/signup", signupValidation, authController.signup);
+router.post("/login", loginValidation, authController.login);
 
 module.exports = router;
