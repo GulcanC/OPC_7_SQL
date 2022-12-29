cf91b115742da9a486d7822c6947d613321c2d73
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2022-12-29 10:59:34 +0100

    create modal

---
+++ frontend/src/components/TokenModal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState, useEffect } from "react";
+// import react bootstrap modal component
+import Modal from "react-bootstrap/Modal";
+
+const TokenModal = ({ setToken }) => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    !show && setToken(false);
+  }, [setToken, show]);
+  return (
+    <Modal
+      show={show}
+      onHide={() => setShow(false)}
+      backdrop="static"
+      keyboard={false}
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Your session has expired</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Please log in again!</Modal.Body>
+    </Modal>
+  );
+};
+
+export default TokenModal;
